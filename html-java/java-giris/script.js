43bdc70a6b34ec7847d29d93965c8d46e79cad6d
OZEN1112 <yoz6441@gmail.com>
2023-11-16 19:12:10 +0100

    commit1

---
+++ html-java/java-giris/script.js
@@ -0,0 +1,34 @@
+
+const p="PROJE-";
+const r="B";
+
+let s=p+r;
+console.log(s);
+
+
+const ad="Yücel";
+const soyad="Özen";
+
+let val=ad+soyad;
+console.log(val);
+
+const b="PROJE-";
+const a="A";
+
+let t=b+a;
+console.log(t);
+
+
+
+let x=2;
+    y=2;
+    
+    
+while(x<101){
+
+    console.log(x);
+    
+    x=x+y;
+}
+
+
